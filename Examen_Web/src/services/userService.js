8acc038a1d5351393aca87ce4814fac175839703
Adali Carvajal <77343598+adalicarvajal@users.noreply.github.com>
2024-02-07 08:09:44 -0500

    Delete Examen_Web directory

--- Examen_Web/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import { supabase } from '../supabase/client';
-
-export const getTotalUsers = async () => {
-    const { data, error } = await supabase.from('users').select('id');
-    if (error) {
-        throw new Error(error);
-    }
-    return data.length;
-}
